34ad494c5d6ef738ae661a5da767061d17254924
Taras <tariksyurts@gmail.com>
2021-12-14 20:41:03 +0200

    rsa done 2

--- src/rsa/rsa.js
+++ src/rsa/rsa.js
@@ -3,11 +3,11 @@
 function generate(keysize) {
 
     function random_prime(bits) {
-        var min = bigInt(6074001000).shiftLeft(bits - 33); // min ≈ √2 × 2^(bits - 1)
-        var max = bigInt.one.shiftLeft(bits).minus(1);   // max = 2^(bits) - 1
+        var min = bigInt(6074001000).shiftLeft(bits - 33)
+        var max = bigInt.one.shiftLeft(bits).minus(1)
         while (true) {
-            var p = bigInt.randBetween(min, max);  // WARNING: not a cryptographically secure RNG!
-            if (p.isProbablePrime(256)) return p;
+            var p = bigInt.randBetween(min, max)
+            if (p.isProbablePrime(256)) return p
         }
     }
 
@@ -16,10 +16,10 @@ function generate(keysize) {
 
 
     do {
-        p = random_prime(keysize / 2);
-        q = random_prime(keysize / 2);
-        lambda = bigInt.lcm(p.minus(1), q.minus(1));
-    } while (bigInt.gcd(e, lambda).notEquals(1) || p.minus(q).abs().shiftRight(keysize / 2 - 100).isZero());
+        p = random_prime(keysize / 2)
+        q = random_prime(keysize / 2)
+        lambda = bigInt.lcm(p.minus(1), q.minus(1))
+    } while (bigInt.gcd(e, lambda).notEquals(1) || p.minus(q).abs().shiftRight(keysize / 2 - 100).isZero())
 
     return {
         n: p.multiply(q),
@@ -30,12 +30,12 @@ function generate(keysize) {
 
 
 function encrypt(m, n, e) {
-    return bigInt(m).modPow(e, n);
+    return bigInt(m).modPow(e, n)
 }
 
 
 function decrypt(c, d, n) {
-    return bigInt(c).modPow(d, n);
+    return bigInt(c).modPow(d, n)
 }
 
 export { 
